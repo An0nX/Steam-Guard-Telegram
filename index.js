9151a10152bbe9f0c9d87047c54f583fd0ef1a1f
httpshotmaker <80145620+httpshotmaker@users.noreply.github.com>
2023-01-07 14:52:50 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+const TelegramBot = require('node-telegram-bot-api');
+const SteamTotp = require('steam-totp');
+
+// устанавливаем токен бота
+const token = process.env['TELEGRAM_BOT_TOKEN'];
+
+// создаем экземпляр бота
+const bot = new TelegramBot(token, { polling: true });
+
+// устанавливаем shared_secret из аккаунта Steam
+const sharedSecret = process.env['shared_secret'];
+
+let messageId = null; // храним id последнего сообщения с кодом Steam Guard
+
+// определяем функцию для обновления сообщения с кодом Steam Guard
+async function updateCode() {
+  // генерируем код Steam Guard
+  const code = SteamTotp.getAuthCode(sharedSecret);
+  // формируем сообщение с моноширинным шрифтом для кода Steam Guard
+  const message = 'Ваш код Steam Guard:\n' + '```\n' + code + '\n```';
+
+  try {
+    if (messageId) {
+      // если id последнего сообщения существует, пытаемся обновить сообщение
+      await bot.editMessageText(message, {
+        chat_id: process.env['user_id'],
+        message_id: messageId,
+        parse_mode: 'Markdown',
+      });
+    } else {
+      // иначе отправляем новое сообщение
+      const sentMessage = await bot.sendMessage(process.env['user_id'], message, { parse_mode: 'Markdown' });
+      messageId = sentMessage.message_id; // сохраняем id отправленного сообщения
+    }
+  } catch (error) {
+    // если произошла ошибка при редактировании сообщения, удаляем старое сообщение
+    if (messageId) {
+      await bot.deleteMessage(process.env['user_id'], messageId);
+      messageId = null;
+    }
+    // и отправляем новое сообщение
+    const sentMessage = await bot.sendMessage(process.env['user_id'], message, { parse_mode: 'Markdown' });
+    messageId = sentMessage.message_id; // сохраняем id отправленного 
+  }
+}
+    // запускаем функцию отправки сообщения с кодом Steam Guard раз в 30 секунд
+    setInterval(updateCode, 30000);
